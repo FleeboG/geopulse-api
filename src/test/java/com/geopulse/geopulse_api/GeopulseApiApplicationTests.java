bf9281f2d2cb07953bce33ff1629f5b41b814dfc
Ernesto Sierra <ernestosierra131@yahoo.com>
2026-02-18 21:33:35 -0600

    test: fix contextLoads and use H2 test profile

--- src/test/java/com/geopulse/geopulse_api/GeopulseApiApplicationTests.java
+++ src/test/java/com/geopulse/geopulse_api/GeopulseApiApplicationTests.java
@@ -2,12 +2,13 @@ package com.geopulse.geopulse_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class GeopulseApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
